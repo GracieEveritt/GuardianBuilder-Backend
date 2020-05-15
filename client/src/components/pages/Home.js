30ad2dc2cfd34238314aac98b21bdc0408cac8cd
GracieEveritt <gracie@feltapp.com>
2020-05-15 11:32:54 -0600

    dashboard updates

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -10,9 +10,15 @@ const Home = (props) => {
         authContext.loadAccount();
         //eslint-disable-next-line
     }, [])
-
+    const reroute = () =>{
+        props.history.push('/forms')
+    }
     return(
-        <h1>Dashboard</h1>
+        <div className='dashboard'>
+            <h1>Welcome! Let's create a legal document!</h1>
+            <button onClick={reroute} className='create-form-button btn btn-dark bt-sm'>Start New Form</button> 
+        </div>
+
     )
 }
 
