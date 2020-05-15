8b789ed4d35dea741d8234d929b5eca85256fb7d
GracieEveritt <gracie@feltapp.com>
2020-05-15 10:41:16 -0600

    deploy fix

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const path = require('path')
 const morgan = require('morgan')
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 connectDB();
 
@@ -15,9 +14,6 @@ app.use(parser.json());
 app.use(cors());
 app.use(morgan('dev'))
 
-//remove this when deploy
-// app.get('/', (req, res) => res.json({msg:'Welcome to the GuardianBuilder API'}));
-
 app.use('/api/accounts', require('./routes/accounts'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/forms', require('./routes/forms'));
@@ -25,7 +21,9 @@ app.use('/api/child', require('./routes/child'));
 app.use('/api/parent', require('./routes/parent'));
 app.use('/api/guardian', require('./routes/guardian'));
 
-if(process.env.NODE_env === 'production') {
+const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*',(req,res) => res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html')))
 }
